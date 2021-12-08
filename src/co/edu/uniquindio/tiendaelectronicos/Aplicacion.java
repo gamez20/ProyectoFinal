72b05851d5bda489aa38353310c2d612c5c8f681
gamez20 <gamez.guerra18@gmail.com>
2021-12-08 12:03:50 -0500

    integracion codigo chiqui

--- src/co/edu/uniquindio/tiendaelectronicos/Aplicacion.java
+++ src/co/edu/uniquindio/tiendaelectronicos/Aplicacion.java
@@ -1,14 +1,40 @@
 package co.edu.uniquindio.tiendaelectronicos;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Aplicacion extends Application {
 
+	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Tienda Electrodomesticos");
+		mostrarVentanaPrincipal();
 	}
+	
+	public void mostrarVentanaPrincipal() {
+
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Aplicacion.class.getResource("view/Plantilla.fxml"));
+			AnchorPane rootLayout = (AnchorPane)loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
 
 	public static void main(String[] args) {
 		launch(args);
